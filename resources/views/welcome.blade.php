485632efb0993cb191f2e3f366d6960a92613f39
JayanUpasara <132430387+JayanUpasara@users.noreply.github.com>
2023-08-24 21:57:52 +0530

    second commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,221 @@
+<html>
+    <head>
+        <title>Train Ticket Reservation</title>
+        <link rel="stylesheet" href="{{asset('cssfile/style.css')}}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <meta name="viewport" content="width = device-width, initial scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+
+        <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    </head>
+    <body>
+        <section id="banner">
+            <img src="images/logo SRILANKA TRAINS.png" class="logo">
+            <div class="banner-text">
+                <h1>SRILANKA TRAINS</h1>
+                <br>
+                <p>Choose the most convenint and fascinating way to travel through SriLanka-Train! <br> Find out all you need for your journey, including booking tickets and station information. </p>
+                <div class="banner-btn">
+                    <a href="#"><span></span>Train Schedule</a>
+                    <a href="#"><span></span>Book Tickets</a>
+                </div>
+            </div>
+        </section>
+
+        <div id="sideNav">
+            <nav>
+                <ul>
+                    <li><a href="#banner">HOME</a></li>
+                    <li><a href="#feature">GALLERY</a></li>
+                    <li><a href="#service">POPULAR ROUTES</a></li>
+                    <li><a href="#testimonial">TRAIN CLASSES</a></li>
+                    <li><a href="#footer">TRAIN STATIONS</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="menuBtn">
+            <img src="images/menu.png" id="menu">
+        </div>
+
+        <section id="feature">
+            <div class="title-text">
+                <p>Features</p>
+                <h1>Why Choose Us</h1>
+            </div>
+            <div class="feature-box">
+                <div class="features">
+                    <h1>Experienced Staff</h1>
+                    <div class="features-desc">
+                        <div class="feature-icon">
+                            <i class="fa fa-shield" aria-hidden="true"></i>
+                        </div>
+                        <div class="feature-text">
+                            <p>Donec dnjib kyvdnazs skhiausgbi sjsndksdn</p>
+                        </div>
+                    </div>
+                    <h1>Pre Booking Online</h1>
+                    <div class="features-desc">
+                        <div class="feature-icon">
+                            <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                        </div>
+                        <div class="feature-text">
+                            <p>Donec dnjib kyvdnazs skhiausgbi sjsndksdn</p>
+                        </div>
+                    </div>
+                    <h1>Affordable Cost</h1>
+                    <div class="features-desc">
+                        <div class="feature-icon">
+                            <i class="fa fa-inr" aria-hidden="true"></i>
+                        </div>
+                        <div class="feature-text">
+                            <p>Donec dnjib kyvdnazs skhiausgbi sjsndksdn</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="features-img">
+                    <img src="images/Elle railway station.jpeg">
+                </div>
+            </div>
+        </section>
+
+        <section id="service">
+            <div class="title-text">
+                <p>SERVICES</p>
+                <h1>We Provide Better</h1>
+            </div>
+            <div class="service-box">
+                <div class="single-service">
+                    <img src="images/nine arch.jpg">
+                    <div class="overlay"></div>
+                    <div class="service-desc">
+                        <h3>First CLass</h3>
+                        <hr>
+                        <p>This is first class this is first class</p>
+                    </div>
+                </div>
+                <div class="single-service">
+                    <img src="images/red train.jpg">
+                    <div class="overlay"></div>
+                    <div class="service-desc">
+                        <h3>First CLass</h3>
+                        <hr>
+                        <p>This is first class this is first class</p>
+                    </div>
+                </div>
+                <div class="single-service">
+                    <img src="images/train in hill station.jpg">
+                    <div class="overlay"></div>
+                    <div class="service-desc">
+                        <h3>First CLass</h3>
+                        <hr>
+                        <p>This is first class this is first class</p>
+                    </div>
+                </div>
+                <div class="single-service">
+                    <img src="images/train in hill.jpeg">
+                    <div class="overlay"></div>
+                    <div class="service-desc">
+                        <h3>First CLass</h3>
+                        <hr>
+                        <p>This is first class this is first class</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+            
+        <section id="testimonial">
+            <div class="title-text">
+                <p>SERVICES</p>
+                <h1>We Provide Better</h1>
+            </div>
+
+            <div class="testimonial-row">
+                <div class="testimonial-col">
+                    <div class="user">
+                        <img src="images/user 1 front-view-man-with-nose-ring.jpg">
+                        <div class="user-info">
+                            <h4>KEN BYTE<i class="fa fa-twitter" aria-hidden="true"></i></h4>
+                            <small>@kenbyte</small>
+                        </div>
+                    </div>
+                    <p>Donmewjnw jbsuiahbsda jahiuhA HUJSBGASDVBWD  sbhsugew jhndoiawh asdbasdnh wssdnwoid sdnosadj idjsaoidj sdnsaoidn skndiosa jsdnoaid</p>
+                </div>
+                <div class="testimonial-col">
+                    <div class="user">
+                        <img src="images/user 2 portrait-white-man-isolated.jpg">
+                        <div class="user-info">
+                            <h4>KEN BYTE<i class="fa fa-twitter" aria-hidden="true"></i></h4>
+                            <small>@kenbyte</small>
+                        </div>
+                    </div>
+                    <p>Donmewjnw jbsuiahbsda jahiuhA HUJSBGASDVBWD  sbhsugew jhndoiawh asdbasdnh wssdnwoid sdnosadj idjsaoidj sdnsaoidn skndiosa jsdnoaid</p>
+                </div>
+                <div class="testimonial-col">
+                    <div class="user">
+                        <img src="images/user 3 close-up-portrait-curly-handsome-european-male.jpg">
+                        <div class="user-info">
+                            <h4>KEN BYTE<i class="fa fa-twitter" aria-hidden="true"></i></h4>
+                            <small>@kenbyte</small>
+                        </div>
+                    </div>
+                    <p>Donmewjnw jbsuiahbsda jahiuhA HUJSBGASDVBWD  sbhsugew jhndoiawh asdbasdnh wssdnwoid sdnosadj idjsaoidj sdnsaoidn skndiosa jsdnoaid</p>
+                </div>
+        </div>
+        </section>
+
+        <section id="footer">
+            <img src="images/footer tourists train.webp" class="footer-img">
+            <div class="title-text">
+                <p>Contact</p>
+                <h1>We Provide Better</h1>
+            </div>
+            <div class="footer-row">
+                <div class="footer-left">
+                    <h1>opening hours</h1>
+                    <p><i class="fa fa-clock-o" aria-hidden="true"></i>Monday to firday 9-9</p>
+                    <p><i class="fa fa-clock-o" aria-hidden="true"></i>Monday to firday 9-9</p>
+                </div>
+                <div class="footer-right">
+                    <h1>get in touch</h1>
+                    <p>339, pasndi, colombo<i class="fa fa-map-marker" aria-hidden="true"></i></p>
+                    <p>jay@gmail.com<i class="fa fa-paper-plane" aria-hidden="true"></i></p>
+                    <p>+945625362<i class="fa fa-phone" aria-hidden="true"></i></p>
+                </div>
+            </div>
+            <div class="social-links">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                <p>copyright srilanka trains</p>
+            </div>
+        </section>
+
+        <script>
+            var menuBtn = document.getElementById("menuBtn")
+            var sideNav = document.getElementById("sideNav")
+            var menu = document.getElementById("menu")
+
+            sideNav.style.right = "-250px";
+
+            menuBtn.onclick = function(){
+                if(sideNav.style.right == "-250px"){
+                    sideNav.style.right = "0";
+                    menu.src = "images/close.png";
+                }
+                else{
+                    sideNav.style.right = "-250px";
+                    menu.src = "images/menu.png";
+                }
+            }
+
+            var scroll = new SmoothScroll('a[href*="#"]', {
+	            speed: 1000,
+	            speedAsDuration: true
+            });
+        </script>
+    </body>
+</html>
